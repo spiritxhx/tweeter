65e4bcbaa24480e03af6bacf0a2f797c023c02d0
spiritxhx <spiritxhx@gmail.com>
2019-07-16 17:21:13 -0400

    add the article css and add font-awsome

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
-  $('textarea').on('keydown', function (event) {
-    let num = $('textarea').val().length;
+  let num = 0;
+  $('textarea').on('input', function () {
+    num = $(this).val().length;
     $(this).closest('section').find('span').html(`${140 - num}`);
+    if (num > 140) {
+      // $(this).closest('section').find('span').html(`Input oversized!`);
+      $(this).closest('section').find('span').css({ 'color': 'red' });
+    }
     // console.log(num);
   })
 
